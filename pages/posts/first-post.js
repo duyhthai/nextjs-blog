5c9a527e448bb9c7ccfe6d81bd74593b0ee9a70f
Duy Thai <duy.thai@altsourcesoftware.com>
2022-02-11 17:45:59 +0700

    nextjs basics

---
+++ pages/posts/first-post.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/layout";
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      // props for your component
+    },
+  };
+}
+
+export default function FirstPost() {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
+        <h1>First Post</h1>
+      </Layout>
+    </>
+  );
+}
